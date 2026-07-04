3648c1b6da19681eeb218b81fd58b863e9b76a59
Diwakar Singh <diwakarsingh@Diwakars-MacBook-Air.local>
2026-07-04 12:31:00 +0530

    Add 26-Remove-duplicates-from-sorted-array

---
+++ 0141-linked-list-cycle/26-Remove-duplicates-from-sorted-array/26-Remove-duplicates-from-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 1){
+            return 1;
+        }
+        int slow =0;
+        int fast =1;
+        while(fast < nums.length){
+            if(nums[fast] != nums[slow]){
+                nums[slow + 1] = nums[fast];
+                slow++;
+            }
+            fast++;
+            
+        }
+        return slow + 1;
+    }
+}
